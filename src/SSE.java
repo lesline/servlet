3cd980ab5f337eb51ac941858883c14028519bfe
zhangshaolin <zhangshaolin1@guazi.com>
2018-09-03 17:33:02 +0800

    Initial commit

---
+++ src/SSE.java
@@ -0,0 +1,46 @@
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Servlet implementation class SSE
+ */
+@WebServlet("/sse/url")
+public class SSE extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public SSE() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/event-stream");
+		response.setCharacterEncoding("utf-8");
+
+		for (int i = 0; i < 5; i++) {
+			// 指定事件标识
+			response.getWriter().write("event:me\n");
+			// 格式: data: + 数据 + 2个回车
+			response.getWriter().write("data:" + i + "\n\n");
+			response.getWriter().flush();
+
+			try {
+				TimeUnit.SECONDS.sleep(1);
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/readme.txt
@@ -0,0 +1,8 @@
+http://localhost:8080/AsyncServlet
+
+http://localhost:8080/AsyncServlet
+
+http://localhost:8080/SSE
+
+
+https://blog.csdn.net/jesonjoke/article/details/78276714
